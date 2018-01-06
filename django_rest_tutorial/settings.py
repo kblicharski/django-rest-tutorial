8aae2413129ea3e9196c2718b8d2a34db54fa04d
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2018-01-05 22:31:55 -0600

    Add app boilerplate and a Snippet model.

--- django_rest_tutorial/settings.py
+++ django_rest_tutorial/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
+    'snippets.apps.SnippetsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- django_rest_tutorial/urls.py
+++ django_rest_tutorial/urls.py
@@ -14,29 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib.auth.models import User
 from django.urls import include
-# Serializers define the API representation.
-from rest_framework import routers, serializers, viewsets
-
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    # url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
