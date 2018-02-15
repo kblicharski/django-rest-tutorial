51c35a5510102d5efb8949759e66a7842a5672cb
Kevin Blicharski <blicharskikevin@gmail.com>
2018-02-15 16:43:22 -0600

    Add an example of DRF views

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+from .models import LANGUAGE_CHOICES, STYLE_CHOICES, Snippet
 
 
 class SnippetSerializer(serializers.Serializer):
